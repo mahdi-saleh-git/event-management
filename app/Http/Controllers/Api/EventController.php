3ce1d1287d2403ad6e7d5d680e63bfde959903e4
mahdi-saleh-git <salehnurmahdiah@gmail.com>
2025-08-14 13:07:40 +0800

    Event controller changes

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
@@ -12,7 +13,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return Event::all();
     }
 
     /**
@@ -20,30 +21,55 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = Event::create([
+            ...$request->validate([
+                'name' => 'required|max:225',
+                'description' => 'nullable|max:1000',
+                'start_at' => 'required|date|after_or_equal:today',
+                'end_at' => 'required|date|after:start_at',
+            ]),
+            'user_id' => 1,
+        ]);
+
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update(
+            $request->validate([
+                'name' => 'sometimes|max:225',
+                'description' => 'nullable|max:1000',
+                'start_at' => 'sometimes|date',
+                'end_at' => 'sometimes|date|after:start_at',
+            ])
+            );
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+
+        // return response(status : 204);
+
+        return response()->json([
+            "message" => "Delete Success"
+        ]);
+
     }
 }
